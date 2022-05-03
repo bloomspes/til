25673dd441e7d7f0e662d7894d3659e854c42424
Spes <bloomspes@gmail.com>
2022-05-03 13:47:36 +0900

    modify: void type for method `on`

--- node/stream.js
+++ node/stream.js
@@ -2,10 +2,11 @@ const { log } = console;
 
 // Create a stream object.
 // The `stream` module provides an API for implementing the stream interface.
-
 const { Stream } = require("stream");
 const writableStream = new Stream.Writable()
 const readableStream =  new Stream.Readable({
+    // If the `readable.read()` method returns a chunk of data, 
+    // a `'data'` event willalso be emitted.
     read() {}
 })
 
@@ -22,7 +23,7 @@ readableStream.push('hi')
 readableStream.push('file system')
 
 readableStream.on('close', () => writableStream.end())
-writableStream.on('close', (stream) => log('ended'))
+writableStream.on('close', () => log('ended'))
 
 // Destroy the stream. 
 // Implementors should not override this method, but instead implement `readable._destroy()`.
