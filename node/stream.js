3b1118c728d4b62bc1d2c4f37cdff6268cf93b92
Spes <bloomspes@gmail.com>
2022-05-03 13:44:08 +0900

    Added references

--- node/stream.js
+++ node/stream.js
@@ -1,19 +1,21 @@
 const { log } = console;
 
-// create a stream object
+// Create a stream object.
+// The `stream` module provides an API for implementing the stream interface.
+
 const { Stream } = require("stream");
 const writableStream = new Stream.Writable()
 const readableStream =  new Stream.Readable({
     read() {}
 })
 
-// implement _write
+// Implement _write.
 writableStream._write = (chunk, encoding, next) => {
     log(chunk.toString())
     next()
 }
 
-// reading a data from a readable stream
+// Reading a data from a readable stream.
 readableStream.pipe(writableStream)
 
 readableStream.push('hi')
@@ -22,4 +24,6 @@ readableStream.push('file system')
 readableStream.on('close', () => writableStream.end())
 writableStream.on('close', (stream) => log('ended'))
 
+// Destroy the stream. 
+// Implementors should not override this method, but instead implement `readable._destroy()`.
 readableStream.destroy()
